ae59b77c7b97caa92daa21fd4d6b5ee7a4eba9cd
StephenTrainor <setrain@seas.upenn.edu>
2025-01-07 20:44:24 -0800

    Update TemplateItem to handle new radio template field

--- components/TemplateItem.tsx
+++ components/TemplateItem.tsx
@@ -1,9 +1,29 @@
 import { TemplateField } from "@/types/templateField";
+import Date from "./Date";
+import Radio from "./Radio";
+import Number from "./Number";
+import Slider from "./Slider";
+import Dropdown from "./Dropdown";
+import CheckBoxGroup from "./CheckBoxGroup";
+import MultiLineText from "./MultiLineText";
+import SingleLineText from "./SingleLineText";
 
 const TemplateItem = ({ templateField } : {templateField: TemplateField}) => {
-  return (
-    <div>
+  const components = {
+    "checkBoxGroup": <CheckBoxGroup answerOptions={templateField.answerOptions}/>,
+    "multiLineText": <MultiLineText answerOptions={templateField.answerOptions}/>,
+    "singleLineText": <SingleLineText answerOptions={templateField.answerOptions}/>,
+    "number": <Number answerOptions={templateField.answerOptions}/>,
+    "radio": <Radio answerOptions={templateField .answerOptions}/>,
+    "date": <Date answerOptions={templateField.answerOptions}/>,
+    "slider": <Slider answerOptions={templateField.answerOptions}/>,
+    "dropdown": <Dropdown answerOptions={templateField.answerOptions}/>,
+  };
 
+  return (
+    <div className="">
+      <p className="pb-1">{templateField.fieldTitle}</p>
+      {components[templateField.fieldType]}
     </div>
   );
 };
