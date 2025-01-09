dcf58efa61577575ad5a9784aad73440e3d29576
StephenTrainor <setrain@seas.upenn.edu>
2025-01-08 22:17:43 -0800

    Added an internal state for each checkbox item, independent of other boxes

--- components/CheckBoxItem.tsx
+++ components/CheckBoxItem.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
+
 const CheckBoxItem = ({ checkBoxText } : { checkBoxText: string }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
-    <div className="flex1 button-outline py-1">{checkBoxText}</div>
+    <button className={isChecked ? "flex1 button-light-blue round-soft py-1" : "flex1 button-outline bg-white py-1"} onClick={() => setIsChecked(!isChecked)}>
+      {checkBoxText}
+    </button>
   );
 };
 
