be5adfb5e3adf7fb33bbbfface48f3032a48e667
StephenTrainor <setrain@seas.upenn.edu>
2025-01-08 19:56:00 -0800

    Update button component to accept an optional onClick function

--- components/OutlineButtonWithIcon.tsx
+++ components/OutlineButtonWithIcon.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 
 const OutlineButtonWithIcon = ({
-  buttonText, buttonClass, classes, src, size, alt
+  buttonText, buttonClass, classes, src, size, alt, onClick
 } : {
-  buttonText: string, buttonClass: string, classes: string, src: string, size: number, alt: string
+  buttonText: string, buttonClass: string, classes: string, src: string, size: number, alt: string, onClick: () => void
 }) => {
   return (
     <div className={`button-outline py-1 px-3 ${buttonClass}`}>
-      <button>{buttonText}</button>
+      <button onClick={() => onClick()}>{buttonText}</button>
       <Image 
         className={`self-center ml-1 ${classes}`}
         src={src}
@@ -20,6 +20,7 @@ const OutlineButtonWithIcon = ({
 };
 
 OutlineButtonWithIcon.defaultProps = {
+  onClick: () => {},
   classes: "",
   buttonClass: "",
 }
