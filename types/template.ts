151a2017e8b1a9d1a6f51e32655f53f2d64596c9
StephenTrainor <setrain@seas.upenn.edu>
2025-01-09 21:40:05 -0800

    Moved all template types to be defined in one file for organization

--- types/template.ts
+++ types/template.ts
@@ -4,3 +4,24 @@ export type Template = {
   author: string;
   imageUrl: string;
 }
+
+export type TemplateSection  = {
+  sectionName: string;
+  sectionDescription: string;
+  sectionItems: TemplateSectionItem[];
+}
+
+export type TemplateSectionItem = {
+  answerOptions: string[];
+  fieldTitle: string;
+  fieldDetails: string;
+  fieldType: "singleLineText" |
+      "multiLineText" |
+      "number" |
+      "date" |
+      "slider" |
+      "radio" |
+      "checkBoxGroup"
+}
+
+
